3c373a49f9bfd609ac07bd42bef888d0c7459a9b
Aremu David <55801713+Khingdave1@users.noreply.github.com>
2020-07-13 23:28:22 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+    // MENU //
+const menu = document.getElementById('mobile-menu');
+const head = document.querySelector('header');
+
+menu.addEventListener('click', function(){
+    head.classList.toggle('open-menu');
+});
+
+// SLIDE //
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("testi_item");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
